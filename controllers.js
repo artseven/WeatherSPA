caa18bd9a1f2760e15d24808b063385a7cec36bb
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-16 14:29:30 -0500

    adding ngSubmit

--- controllers.js
+++ controllers.js
@@ -1,31 +1,34 @@
 // CONTROLLERS
 weatherApp.controller('homeController', [
-    '$scope','cityService',
-function ($scope, cityService) {
+            '$scope','$location','cityService',
+    function ($scope, $location, cityService) {
 
-$scope.city = cityService.city;
-$scope.$watch('city', function() {
-   cityService.city = $scope.city;
-});
+    $scope.city = cityService.city;
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+    $scope.submit = function() {
+        $location.path("/forecast");
+    };
 }]);
 
 weatherApp.controller('forecastController', [
-    '$scope','$resource','$routeParams','cityService',
-function ($scope, $resource, $routeParams, cityService) {
+             '$scope','$resource','$routeParams','cityService',
+    function ($scope, $resource, $routeParams, cityService) {
 
-$scope.city = cityService.city;
+    $scope.city = cityService.city;
 
-$scope.days = $routeParams.days || '2';
+    $scope.days = $routeParams.days || '2';
 
-$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=a0be02eb7e1ad8c86374d21ee734d432");
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=a0be02eb7e1ad8c86374d21ee734d432");
 
-$scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
 
-$scope.convertToFahrenheit = function(degK) {
-   return Math.round((1.8 * (degK-273)) + 32);
-};
+    $scope.convertToFahrenheit = function(degK) {
+       return Math.round((1.8 * (degK-273)) + 32);
+    };
 
-$scope.convertToDate = function(dt) {
-   return new Date(dt*1000);
-};
+    $scope.convertToDate = function(dt) {
+       return new Date(dt*1000);
+    };
 }]);
